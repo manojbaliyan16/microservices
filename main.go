e1241ac7a0242834b0d662c13cd442fad519944e
Manoj Kumar <manoj.kumar@rain.us>
2022-08-07 00:58:43 +0530

    Customized handler, multipleHandler, Shopisticated Server

--- main.go
+++ main.go
@@ -5,23 +5,40 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	"microservices/handlers"
 )
 
 func main() {
-	fmt.Println("Hello Go world")
+	// As we are going to use our own handler so below one will be commented
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Default handler")
+		log.Println("Handling the client request and ready for Response ")
 		d, err := ioutil.ReadAll(r.Body)
-		fmt.Printf("The data is %s\n", d)
 		if err != nil {
-			http.Error(w, "oops Error", http.StatusBadRequest)
-			return
+			log.Println(http.StatusBadRequest)
 		}
 		fmt.Fprintf(w, "hello %s", d)
-	})
-	http.HandleFunc("/GoodBye", func(http.ResponseWriter, *http.Request) {
-		log.Println("Bbye byt to C++ ")
-	})
-	http.ListenAndServe(":9090", nil)
 
+	})
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	log.Println("Start the Server")
+	s := &http.Server{
+		Addr:         ":3000",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	log.Fatal(s.ListenAndServe())
+	log.Println("Server Started ")
 }
