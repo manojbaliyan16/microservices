0eb69d23b8849b3cb44799d92c8403ca4db1489e
Manoj Kumar <manoj.kumar@rain.us>
2022-08-08 00:15:52 +0530

    Impelemeting the GET request Method

--- handlers/product.go
+++ handlers/product.go
@@ -18,12 +18,19 @@ func NewProducts(l *log.Logger) *Products {
 func (p *Products) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	//need to create a GET request which will return a product define in to product/product.go
 	log.Println("Going to handle products of all types ")
-	lp := data.GetProducts() // here Lp is having the list of product now how can we return it to the user The way is to convert it(LP) into JSON can be done by using encoding JSON
-	/*bytes, err := json.Marshal(lp) // bytes--> slice of data
-	if err != nil {
-		http.Error(w, "unable to marshal Json", http.StatusInternalServerError)
+	// based on the HTTP request Method take the decision as below using go standard library
+	if r.Method == http.MethodGet {
+		p.getProducts(w, r)
 	}
-	w.Write(bytes)*/
+	// for anyrthinhg else
+	// catch all
+	w.WriteHeader(http.StatusMethodNotAllowed)
+
+}
+
+func (p *Products) getProducts(w http.ResponseWriter, r *http.Request) {
+	lp := data.GetProducts() // here Lp is having the list of product now how can we return it to the user The way is to convert it(LP) into JSON can be done by using encoding JSON
+	// this Lp is the get product
 	err := lp.ToJSON(w)
 	if err != nil {
 		http.Error(w, "Unable to Encode", http.StatusInternalServerError)
